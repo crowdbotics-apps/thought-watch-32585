136dda7b8cc5116a4a6e62b9982b129cecf7080e
Crowdbotics <team@crowdbotics.com>
2021-12-25 03:18:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "thought_watch_32585",
+  url: "https://thought_watch_32585.botics.co",
+  api: "https://thought_watch_32585.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
